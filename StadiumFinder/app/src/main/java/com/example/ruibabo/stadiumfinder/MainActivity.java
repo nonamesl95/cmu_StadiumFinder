bd6af6343ce164a0d4b5c0affde0018cfed27cf4
jonutefe840 <nunoferreira840@hotmail.com>
2017-11-28 12:07:01 +0000

    Registar

--- StadiumFinder/app/src/main/java/com/example/ruibabo/stadiumfinder/MainActivity.java
+++ StadiumFinder/app/src/main/java/com/example/ruibabo/stadiumfinder/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ruibabo.stadiumfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button regist = findViewById(R.id.registarBtn);
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent int1 = new Intent(MainActivity.this, Registar.class);
+
+                startActivity(int1);
+            }
+        });
     }
 }
